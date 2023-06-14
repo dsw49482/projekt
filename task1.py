df2d1b547b04a12a804305f445824783b5db4842
dsw49482 <135141095+dsw49482@users.noreply.github.com>
2023-06-14 05:58:31 +0200

    task1.py

--- task1.py
+++ task1.py
@@ -1,16 +1,9 @@
 import argparse
 
-# Task 1: Parsowanie argumentów przekazywanych przy uruchomieniu programu
-parser = argparse.ArgumentParser(description='Program do konwersji danych obsługujący formaty .xml, .json i .yml (.yaml)')
-parser.add_argument('--input', help='Ścieżka pliku wejściowego')
-parser.add_argument('--output', help='Ścieżka pliku wyjściowego')
-parser.add_argument('--format', choices=['xml', 'json', 'yaml'], help='Format pliku wejściowego (xml, json, yaml)')
+parser = argparse.ArgumentParser(description='Program do konwersji danych')
+parser.add_argument('input_file', help='Ścieżka pliku wejściowego')
+parser.add_argument('output_file', help='Ścieżka pliku wyjściowego')
 args = parser.parse_args()
 
-input_path = args.input
-output_path = args.output
-input_format = args.format
-
-print(f'Ścieżka pliku wejściowego: {input_path}')
-print(f'Ścieżka pliku wyjściowego: {output_path}')
-print(f'Format pliku wejściowego: {input_format}')
+input_file_path = args.input_file
+output_file_path = args.output_file
