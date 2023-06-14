dd2caff0d1480a45e3829f3e9d5d3d8780e944e7
dsw49482 <135141095+dsw49482@users.noreply.github.com>
2023-06-14 13:39:01 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,100 +1,88 @@
-import argparse
-import json
-import xml.etree.ElementTree as ET
-import yaml
+# Task1: parsowanie argumentów przekazywanych przy uruchomieniu programu
+import sys
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Program do konwersji danych obsługujący formaty: .xml, .json i .yml (.yaml)')
-    parser.add_argument('input_file', help='Ścieżka do pliku wejściowego')
-    parser.add_argument('output_file', help='Ścieżka do pliku wyjściowego')
-    return parser.parse_args()
+    if len(sys.argv) < 3:
+        print("Usage: program.py input_file output_file")
+        sys.exit(1)
 
-def read_json(input_file):
-    with open(input_file, 'r') as file:
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    return input_file, output_file
+
+
+# Task2: wczytywanie danych z pliku JSON
+import json
+
+def parse_json(file_path):
+    with open(file_path, 'r') as file:
         data = json.load(file)
     return data
 
-def write_json(output_file, data):
-    with open(output_file, 'w') as file:
+
+# Task3: zapis danych do pliku w formacie JSON
+def save_json(file_path, data):
+    with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
 
-def read_yaml(input_file):
-    with open(input_file, 'r') as file:
+
+# Task4: wczytywanie danych z pliku YAML
+import yaml
+
+def parse_yaml(file_path):
+    with open(file_path, 'r') as file:
         data = yaml.safe_load(file)
     return data
 
-def write_yaml(output_file, data):
-    with open(output_file, 'w') as file:
+
+# Task5: zapis danych do pliku w formacie YAML
+def save_yaml(file_path, data):
+    with open(file_path, 'w') as file:
         yaml.dump(data, file, default_flow_style=False)
 
-def read_xml(input_file):
-    tree = ET.parse(input_file)
+
+# Task6: wczytywanie danych z pliku XML
+import xml.etree.ElementTree as ET
+
+def parse_xml(file_path):
+    tree = ET.parse(file_path)
     root = tree.getroot()
     return root
 
-def write_xml(output_file, root):
+
+# Task7: zapis danych do pliku w formacie XML
+def save_xml(file_path, root):
     tree = ET.ElementTree(root)
-    tree.write(output_file, encoding='utf-8', xml_declaration=True)
+    with open(file_path, 'wb') as file:
+        tree.write(file, encoding='utf-8', xml_declaration=True)
 
-def convert_xml_to_json(root):
-    data = {}
-    for child in root:
-        if child:
-            data[child.tag] = child.text.strip()
-    return data
 
-def convert_json_to_xml(data):
-    root = ET.Element('root')
-    for key, value in data.items():
-        child = ET.SubElement(root, key)
-        child.text = str(value)
-    return root
+# Główna funkcja programu
+def convert_data(input_file_path, output_file_path):
+    file_extension = input_file_path.split('.')[-1].lower()
 
-def convert_yaml_to_xml(data):
-    root = ET.Element('root')
-    for key, value in data.items():
-        child = ET.SubElement(root, key)
-        child.text = str(value)
-    return root
-
-def main():
-    args = parse_arguments()
-
-    # Rozpoznawanie formatu pliku na podstawie rozszerzenia
-    input_format = args.input_file.split('.')[-1].lower()
-    output_format = args.output_file.split('.')[-1].lower()
-
-    if input_format == 'json':
-        data = read_json(args.input_file)
-    elif input_format == 'yaml' or input_format == 'yml':
-        data = read_yaml(args.input_file)
-    elif input_format == 'xml':
-        root = read_xml(args.input_file)
+    if file_extension == 'json':
+        # Task2: wczytywanie danych z pliku JSON
+        data = parse_json(input_file_path)
+        # Task3: zapis danych do pliku w formacie JSON
+        save_json(output_file_path, data)
+    elif file_extension in ['yml', 'yaml']:
+        # Task4: wczytywanie danych z pliku YAML
+        data = parse_yaml(input_file_path)
+        # Task5: zapis danych do pliku w formacie YAML
+        save_yaml(output_file_path, data)
+    elif file_extension == 'xml':
+        # Task6: wczytywanie danych z pliku XML
+        root = parse_xml(input_file_path)
+        # Task7: zapis danych do pliku w formacie XML
+        save_xml(output_file_path, root)
     else:
-        print(f'Nieobsługiwany format pliku wejściowego: {input_format}')
+        print(f"Unsupported file format: {file_extension}")
         return
 
-    if output_format == 'json':
-        if input_format == 'xml':
-            # Konwersja danych z XML do JSON
-            data = convert_xml_to_json(root)
-        write_json(args.output_file, data)
-    elif output_format == 'yaml' or output_format == 'yml':
-        if input_format == 'xml':
-            # Konwersja danych z XML do YAML
-            data = convert_xml_to_json(root)
-            data = yaml.dump(data)
-        write_yaml(args.output_file, data)
-    elif output_format == 'xml':
-        if input_format == 'json':
-            # Konwersja danych z JSON do XML
-            root = convert_json_to_xml(data)
-        elif input_format == 'yaml' or input_format == 'yml':
-            # Konwersja danych z YAML do XML
-            root = convert_yaml_to_xml(data)
-        write_xml(args.output_file, root)
-    else:
-        print(f'Nieobsługiwany format pliku wyjściowego: {output_format}')
+    print(f"Data converted successfully. Saved to {output_file_path}")
+
 
 if __name__ == '__main__':
-    main()
+    input_file, output_file = parse_arguments()
+    convert_data(input_file, output_file)
